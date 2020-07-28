e292f0e186cca3720085653d3451522b4dead8dd
Walter Traspadini <walter.traspadini@sorint.it>
2020-07-28 18:05:09 +0200

    Update README and integration with cosmosdb

--- MyFunctionProj/HttpTrigger/__init__.py
+++ MyFunctionProj/HttpTrigger/__init__.py
@@ -1,24 +1,70 @@
-import logging
 
+import azure.cosmos.documents as documents
+import azure.cosmos.cosmos_client as cosmos_client
+import azure.cosmos.exceptions as exceptions
+from azure.cosmos.partition_key import PartitionKey
 import azure.functions as func
+
+import datetime
+import logging
+import os
+
+
+ACCOUNT_HOST = os.environ.get('COSMOS_ACCOUNT_HOST')
+ACCOUNT_KEY = os.environ.get('COSMOS_ACCOUNT_KEY')
+COSMOS_DATABASE = os.environ.get('COSMOS_DATABASE')
+COSMOS_CONTAINER = os.environ.get('COSMOS_CONTAINER')
+
+
+def read_items(container):
+    logging.info('Reading all items in a container\n')
+
+    item_list = list(container.read_all_items(max_item_count=10))
+
+    logging.info('Found {0} items'.format(item_list.__len__()))
+
+    '''
+    for doc in item_list:
+        print('Item Id: {0}'.format(doc.get('id')))
+    '''
+    return item_list
+
 
 
 def main(req):
+
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    try:
+        client = cosmos_client.CosmosClient(ACCOUNT_HOST, {'masterKey': ACCOUNT_KEY},
+            user_agent="CosmosDBDotnetQuickstart",
+            user_agent_overwrite=True,
+            connection_verify=False)
 
-    if name:
-        return func.HttpResponse("Hello {name}!".format(name=name))
-    else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
-        )
+    except exceptions.CosmosResourceExistsError:
+        logging.error("Error creating client")
+        return func.HttpResponse("Error creating client")
+
+    try:
+        logging.info(COSMOS_DATABASE)
+        db = client.create_database_if_not_exists(id=COSMOS_DATABASE)
+    except  exceptions.CosmosResourceExistsError:
+        logging.error("Error creating database")
+        return func.HttpResponse("Error creating database")
+
+    try:
+        container = db.create_container_if_not_exists(id=COSMOS_CONTAINER,
+            partition_key=PartitionKey(path='/account_number'),
+            offer_throughput=400)
+
+    except exceptions.CosmosResourceExistsError:
+            logging.error('Container with id \'{0}\' was found'.format(COSMOS_CONTAINER))
+            return func.HttpResponse("Error creating container")
+
+    item_list = read_items(container)
+
+    return func.HttpResponse(str(item_list))
+
+
+if __name__ == "__main__":
+    main(None)
--- README.md
+++ README.md
@@ -11,10 +11,10 @@
 sudo docker build -t uolter/azure-functions-core-tools .
 ```
 
-## Working with docker image 
+## Working with docker image
 
 ```
-sudo docker run -it --rm -v $(pwd):/src -p 7071:7071 uolter/azure-functions-core-tools 
+sudo docker run -it --rm -v $(pwd):/src -p 7071:7071 uolter/azure-functions-core-tools
 ```
 
 Once in the docker container:
@@ -23,10 +23,15 @@ Once in the docker container:
 >> cd  MyFunctionProj
 >> pyenv virtualenv 3.7.4 venv
 >> pyenv global  venv
+>> eval "$(pyenv init -)"
 ## Run the function locally
 func host start
 ```
 ---
 
+## Deploy in Azure
 
-
+```
+>> export FUNC_APP_NAME=<function name>
+>> func azure functionapp publish $FUNC_APP_NAME
+```
